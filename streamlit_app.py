ca801ebccc32184c91641be421eaab1ad4c2488b
hasancagrigungor <61599311+hasancagrigungor@users.noreply.github.com>
2022-06-14 17:15:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from passporteye.mrz.image import MRZPipeline
 from passporteye import read_mrz
 import pytesseract
-picture=""
+
 picture = st.camera_input("")
+st.write(picture)
 if len(picture)>0:
   mrz = read_mrz(picture)
   st.write(mrz)
