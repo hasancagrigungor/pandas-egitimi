2af0607c6f844bf17d18a1c9ae278330df22cfd9
hasancagrigungor <61599311+hasancagrigungor@users.noreply.github.com>
2023-05-20 17:29:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,48 @@
+import pandas as pd
+import numpy as np
 import streamlit as st
-import functions as ft
-def giris():
-    users={
-        "merve":"19031903",
-        "nazli":"Q7.ihaber",
-    }
-    st.title("Haber Asistan V 1.2")
-    with st.form("giriş",clear_on_submit=True):
-        username=st.text_input("Kullanıcı Adı")
-        password=st.text_input("Şifre",type="password")
-        buton=st.form_submit_button("Giriş Yap")
-        if buton:
-            if users.get(username)==password:
-                st.session_state["key"]="1"
-                st.experimental_rerun()
-            else:
-                st.error("Kullanıcı Adı veya Şifre Hatalı")
+import yfinance as yf
+
+import datetime as dt
+
+liste={
+    "BTC":"BTC-USD",
+    "ETH":"ETH-USD",
+    "XRP":"XRP-USD",
+    "SHIB":"SHIB-USD"
+}
+
+sec=st.sidebar.selectbox("KriptoPara",liste.keys())
+ticker=liste.get(sec)
+
+def veri(ticker,baslangic="2004-01-01",bitis=dt.datetime.today().date()):
+    df=yf.download(ticker,baslangic,bitis)
+    df=df['Close']
+    st.line_chart(df)
 
 
-        else:
-            st.stop()
+col1,col2,col3=st.columns(3)
+with col1:
+    son30 = st.button("30Gün")
+with col2:
+    son90 = st.button("90Gün")
+with col3:
+    son360 = st.button("Yıl")
 
-if "key" not in st.session_state:
-    giris()
+bugun=dt.datetime.today().date()
 
+
+if son30:
+    baslangic=bugun-dt.timedelta(days=30)
+    bitis=bugun
+elif son90:
+    baslangic = bugun - dt.timedelta(days=90)
+    bitis = bugun
+elif son360:
+    baslangic = bugun - dt.timedelta(days=365)
+    bitis = bugun
 else:
+    baslangic = "2004-01-01"
+    bitis = bugun
 
-    st.button("Yenile")
-
-    ft.topluHaberEkle()
-    gunluk_trends=ft.trendsfull()
-    df=ft.haberGetir()
-
-    st.sidebar.title("Anlık Keyword")
-    gunluk_trends.reverse()
-    for a in gunluk_trends:
-        st.sidebar.error(a[0])
-
-
-    dakika=round(df['kalan'].dt.total_seconds()/60)
-    sozluk = dict(zip(df['Başlık'],dakika))
-
-    col1,col2=st.columns([3,1])
-    with col1:
-        st.subheader("Başlık")
-    with col2:
-        st.subheader("Kalan Süre")
-
-    for haber in sozluk.keys():
-        col1,col2=st.columns([3,1])
-        with col1:
-
-            st.write(haber)
-        with col2:
-
-            st.write(sozluk[haber],"dk")
-        st.markdown("""---""")
-
-
-    hide_streamlit_style = """
-                <style>
-                .viewerBadge_link__1S137 {display:none!important;}
-                .viewerBadge_link__1S137 {visibility: hidden!important;}
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                .viewerBadge_container__1QSob {visibility: hidden!important;}
-
-
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+veri(ticker,baslangic,bitis)
